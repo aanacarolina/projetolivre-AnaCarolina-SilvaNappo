fe3b8c963f8cfa9b36693890603d11a305782406
aanacarolina <79257314+aanacarolina@users.noreply.github.com>
2021-07-06 22:08:35 -0300

    Variavel mongo atlas

--- src/data/database.js
+++ src/data/database.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
+const optionEnv = process.env.DATABASE_URL
+
 const connect = () => { mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
